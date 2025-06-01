335d89a781cd1e37a465d26b6a439a53e3b303eb
jhoany flores <floresjhoany2@gmail.com>
2025-06-01 00:33:42 -0400

    inicio y registro de usuarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: '**', redirectTo: 'login' }             // cualquier otra ruta, redirige al login
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
